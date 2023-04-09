dea0cc8e1fa4701fd2f6d909c39217f5f91dd69b
Joe Moon <moonjose@gmail.com>
2023-04-09 13:15:31 -0500

    Updated EndGame condition logic - includes Tied Game

--- project-ttt/index.js
+++ project-ttt/index.js
@@ -16,7 +16,8 @@ const EndGame = {
 // Board Legend: 0 = Nothing, 1 = X, 2 = O
 const board = (() => {
     // Create Game Board
-    const boardSize = 3;
+    const boardSize = 3;        // This also dictates how many consecutive symbols are required for win condition
+    let freeCells = boardSize * boardSize;
     let board = new Array(boardSize).fill(Mark.None).map(() => new Array(boardSize).fill(Mark.None));
 
     const boardSymbol = (row, col) => {
@@ -40,38 +41,103 @@ const board = (() => {
         board.innerHTML = boardHTML;
     };
 
-    // TODO: Add check - can only add if board[row][col] == Mark.None
-    const addX = (row, col) => { board[row][col] = Mark.X; render(); checkForWinner(); };
-    const addO = (row, col) => { board[row][col] = Mark.O; render(); checkForWinner(); };
+    const addableCell = (row, col) => { return board[row][col] === Mark.None; }
+    const addX = (row, col) => {
+        if (!addableCell(row, col)) { return; }
+        board[row][col] = Mark.X;
+        freeCells--;
+        render();
+        checkForWinner();
+    };
+
+    const addO = (row, col) => {
+        if (!addableCell(row, col)) { return; }
+        board[row][col] = Mark.O;
+        freeCells--;
+        render();
+        checkForWinner();
+    };
 
     const checkForWinner = () => {
-        let winner = EndGame.NoWinner;
-        
-        // Check rows
-        for (row in board) {
-            let marked = board[row][0];     // either X or O
-            if (marked) {
-                for (col in board[row]) {
-                    if (board[row][col] == marked) {
-                        winner = (marked == Mark.X) ? EndGame.X_Winner : EndGame.O_Winner;
-                    }                    
-                    else {
-                        winner = EndGame.NoWinner;
-                        break;
+        let winner          = EndGame.NoWinner;
+        let symbolCounter   = 0;
+
+        // Tied Game check
+        if (!freeCells) {
+            winner = EndGame.TiedGame;
+        }
+        else {
+            // Check rows
+            for (row in board) {
+                let marked = board[row][0];     // either X or O
+                if (marked) {
+                    for (col in board[row]) {
+                        if (board[row][col] == marked) {
+                            symbolCounter++;
+                            if (symbolCounter == boardSize) {
+                                winner = (marked == Mark.X) ? EndGame.X_Winner : EndGame.O_Winner;
+                                break;
+                            }
+                        }                    
+                        else {
+                            winner = EndGame.NoWinner;
+                            break;
+                        }
+                    }
+                }
+                if (winner != EndGame.NoWinner) { break; }
+            }
+
+            if (winner == EndGame.NoWinner) {
+                // Check columns
+                for (let col = 0; col < boardSize; col++) {
+                    symbolCounter = 0;
+                    let marked = board[0][col];     // either X or O
+                    if (marked) {
+                        for (let row = 0; row < boardSize; row++) {
+                            if (board[row][col] == marked) {
+                                symbolCounter++;
+                                if (symbolCounter == boardSize) {
+                                    winner = (marked == Mark.X) ? EndGame.X_Winner : EndGame.O_Winner;
+                                    break;
+                                }
+                            }
+                            else {
+                                winner = EndGame.NoWinner;
+                                break;
+                            }
+                        }
+                    }
+                    if (winner != EndGame.NoWinner) { break; }
+                }
+            }
+
+            if (winner == EndGame.NoWinner) {
+                // Check diagonals: Top left to bottom right
+                symbolCounter = 0;
+                let marked = board[0][0];       // either X or O
+                if (marked) {
+                    for (let grid = 1; grid < boardSize; grid++) {
+                        if (board[grid][grid] == marked) {
+                            winner = (marked == Mark.X) ? EndGame.X_Winner : EndGame.O_Winner;
+                        }
+                        else {
+                            winner = EndGame.NoWinner;
+                            break;
+                        }
                     }
                 }
             }
-        }
 
-        if (winner == EndGame.NoWinner) {
-            // Check columns
-            for (let col = 0; col < boardSize; col++) {
-                let marked = board[0][col];     // either X or O
+            if (winner == EndGame.NoWinner) {
+                // Check diagonals: Top right to bottom left
+                symbolCounter = 0;
+                let marked = board[0][2];       // either X or O
                 if (marked) {
                     for (let row = 0; row < boardSize; row++) {
-                        if (board[row][col] == marked) {
+                        if (board[row][boardSize - 1 - row] == marked) {
                             winner = (marked == Mark.X) ? EndGame.X_Winner : EndGame.O_Winner;
-                        }                    
+                        }
                         else {
                             winner = EndGame.NoWinner;
                             break;
@@ -80,41 +146,6 @@ const board = (() => {
                 }
             }
         }
-        
-        if (winner == EndGame.NoWinner) {
-            // Check diagonals: Top left to bottom right
-            let marked = board[0][0];       // either X or O
-            if (marked) {
-                for (let grid = 1; grid < boardSize; grid++) {
-                    if (board[grid][grid] == marked) {
-                        winner = (marked == Mark.X) ? EndGame.X_Winner : EndGame.O_Winner;
-                    }
-                    else {
-                        winner = EndGame.NoWinner;
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (winner == EndGame.NoWinner) {
-            // Check diagonals: Top right to bottom left
-            let marked = board[0][2];       // either X or O
-            if (marked) {
-                for (let row = 0; row < boardSize; row++) {
-                    if (board[row][boardSize - 1 - row] == marked) {
-                        winner = (marked == Mark.X) ? EndGame.X_Winner : EndGame.O_Winner;
-                    }
-                    else {
-                        winner = EndGame.NoWinner;
-                        break;
-                    }
-                }
-            }
-        }
-
-        // TODO: Tied Game check
-
 
         if (winner != EndGame.NoWinner) {
             console.log("Winner: ", winner);
@@ -127,10 +158,26 @@ const board = (() => {
 
 const test = (() => {
     console.log("---TEST---");
+    // board.addO(0,0);
+    board.addX(0,0);
+    // board.addO(0,1);
+    board.addX(0,1);
     board.addO(0,2);
-    board.addO(1,2);
-    board.addO(2,2);
+    // board.addX(0,2);
 
+    // board.addO(1,0);
+    // board.addX(1,0);
+    board.addO(1,1);
+    // board.addX(1,1);
+    board.addO(1,2);
+    // board.addX(1,2);
+
+    board.addO(2,0);
+    // board.addX(2,0);
+    // board.addO(2,1);
+    // board.addX(2,1);
+    // board.addO(2,2);
+    board.addX(2,2);
 })();
 
 board.render();
